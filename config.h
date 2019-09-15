e13a7a4481e9e633cb7c0a4ed4a126a1ea3ee30e
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2019-09-15 09:23:19 -0700

    Initial commit

---
+++ config.h
@@ -0,0 +1,17 @@
+
+/* user and group to drop privileges to */
+static const char *user  = "kesav";
+static const char *group = "kesav";
+
+static const char *colorname[NUMCOLS] = {
+	/* after initialization */
+	[INIT] =   "#282828", // background
+	/* during input */
+	[INPUT] =  "#d79921", // yellow
+	/* wrong password */
+	[FAILED] = "#cc241d", // red
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
