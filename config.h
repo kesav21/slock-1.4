5a4c372a7fe0a604e867ab81bc15f29127629686
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2019-12-29 00:54:00 -0700

    Updated colors

--- config.h
+++ config.h
@@ -1,19 +1,25 @@
+// vim: set foldmethod=marker:
 
 /* user and group to drop privileges to */
 static const char *user  = "kesav";
 static const char *group = "kesav";
 
+// colors/theme/gruvbox {{{
+
+static const char col_background[] = "#282828";
+static const char col_color1[] = "#cc241d";
+static const char col_color6[] = "#689d6a";
+
+// }}}
+
 static const char *colorname[NUMCOLS] = {
-	/* after initialization */
-	[INIT] =   "#282828", // background
-	/* during input */
-	[INPUT] =  "#d79921", // yellow
-	/* wrong password */
-	[FAILED] = "#cc241d", // red
+	[INIT]   = col_background, // after initialization
+	[INPUT]  = col_color6,     // during input
+	[FAILED] = col_color1,     // wrong password
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
 
 /* allow control key to trigger fail on clear */
 static const int controlkeyclear = 1;
